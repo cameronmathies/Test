956e3a014058205f208812ded3549626f955fd6b
cameronmathies <cmathies@IT202.mr.preble.com>
2015-10-22 13:12:54 -0400

    update

---
+++ Test sound/Assets/Room/Keys.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keys : MonoBehaviour {
+//
+	void Update () {
+//		if (Input.GetKey (KeyCode.N)) 
+//		if (Input.GetKey (KeyCode.C))
+//		if (Input.GetKey (KeyCode.O))
+//		if (Input.GetKey (KeyCode.Q))
+//		//if (Input.GetKey (KeyCode.Escape))
+//		if (Input.GetKey (KeyCode.Q))
+//		if (Input.GetKey (KeyCode.Space))
+
+//		if (Input.GetMouseButton(1)){
+//
+//		}
+}
+}
+//	
